31d131efd7e3df3a3ff4ef603d2725637c2d5555
Denali101 <80551417+Denali101@users.noreply.github.com>
2021-04-06 23:12:41 -0400

    Create Fibbonaci_Sequence_Recursive.py

---
+++ Fibbonaci_Sequence_Recursive.py
@@ -0,0 +1,13 @@
+def fib(n): # Fibbonaci sequence
+if n <= 1:
+	return n
+else:
+	return (fib(n-1) + fib(n=2))
+	
+terms = int(input())
+
+if terms <= 0:
+	print('you stupid')
+else:
+	for i in range(terms):
+		print(fib(i))
